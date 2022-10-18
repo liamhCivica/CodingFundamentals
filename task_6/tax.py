0584b80864d00dbad6fb1189c4c2677c245a634b
liamhcivica <liam.hill@civica.co.uk>
2022-10-18 15:50:20 +0100

    add complex calculation

--- task_6/tax.py
+++ task_6/tax.py
@@ -1,18 +1,34 @@
 
-def getIncomeTax(salary):
+def getIncomeTaxSimple(salary):
+    pta = 11850
+
+    if salary > pta:
+        if salary <= 34500:
+            return salary - salary * 0.20
+        elif salary <= 150000:
+            return salary - salary * 0.40
+        elif salary > 150000:
+            return salary - salary * 0.45
+    return salary
+
+def getIncomeTaxComplex(salary):
     pta = 11850
 
     if salary > pta:
         if salary <= 34500:
             return salary - (salary - pta) * 0.20
         elif salary <= 150000:
-            return salary - (salary - pta) * 0.40
+            return salary - (salary - 34500) * 0.40 + (salary % 34500) * 0.20
         elif salary > 150000:
-            return salary - (salary - pta) * 0.45
+            return salary - ((salary - 150000) * 0.45) + (150000 - 34500) * 0.40 + (34500 - pta) * 0.20
     return salary
 
-print(getIncomeTax(10))
+print(getIncomeTaxSimple(10))
+print(getIncomeTaxSimple(20000))
+print(getIncomeTaxSimple(40000))
+print(getIncomeTaxSimple(200000))
 
-print(getIncomeTax(20000))
-print(getIncomeTax(40000))
-print(getIncomeTax(200000))
+print(getIncomeTaxComplex(10))
+print(getIncomeTaxComplex(20000))
+print(getIncomeTaxComplex(40000))
+print(getIncomeTaxComplex(200000))
