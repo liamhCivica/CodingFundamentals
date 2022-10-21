964ddcca6689f13ff2c7232eef912d9a11d210a8
liamhcivica <liam.hill@civica.co.uk>
2022-10-21 14:20:58 +0100

    add overides

--- classexample.py
+++ classexample.py
@@ -1,5 +1,5 @@
-class Snake:
-
+class Reptile:
+    
     def __init__(self, health, colour, x, y):
         self.health = health
         self.colour = colour
@@ -15,10 +15,34 @@ class Snake:
     def __str__(self):
         return f"health: {self.health} colour: {self.colour} x: {self.x} y: {self.y}"
 
+class Snake(Reptile):
+
+    def __init__(self, health, colour, x, y, length):
+        super().__init__(health, colour, x, y)
+        self.length = length
+
+    def print(self):
+        print(f"health: {self.health} colour: {self.colour} x: {self.x} y: {self.y} length: {self.length}")
+
+    def __str__(self):
+        return super().__str__() + f" length: {self.length} "
+
+class Python(Snake):
+
+    def __init__(self, health, colour, x, y, length, origin):
+        super().__init__(health, colour, x, y, length)
+        self.origin = origin
+
+    def print(self):
+        print(f"health: {self.health} colour: {self.colour} x: {self.x} y: {self.y} length: {self.length} origin: {self.origin}")
+
+    def __str__(self):
+        return super().__str__() + f" origin: {self.origin} "
+
 snakes = []
 
 for i in range(10):
-    newSnake = Snake(100, "red", i, i)
+    newSnake = Snake(100, "red", i, i, 2)
     snakes.append(newSnake)
 
 for snake in snakes:
@@ -38,3 +62,6 @@ if not hasNew:
     print(getattr(snakes[0], newAttribute))
     delattr(snakes[0], newAttribute)
 
+python = Python(100, "green", 1, 1, 20, "africa")
+
+print(python)
